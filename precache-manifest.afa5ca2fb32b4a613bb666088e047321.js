c250f2ea89b33d00a1d6774e0152151403a7adc1
Nathan Beck <nathan.beck@fleishman.com>
2018-12-21 15:57:36 -0600

    Updates

---
+++ precache-manifest.afa5ca2fb32b4a613bb666088e047321.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1630c2ed742f926e7fded2eae58687ee",
+    "url": "/lets-watch-sailormoon/static/media/sailor_moon_logo.1630c2ed.png"
+  },
+  {
+    "revision": "da730410a9345ec6e238",
+    "url": "/lets-watch-sailormoon/static/js/runtime~main.da730410.js"
+  },
+  {
+    "revision": "3fb44188e03cb1fd042a",
+    "url": "/lets-watch-sailormoon/static/js/main.3fb44188.chunk.js"
+  },
+  {
+    "revision": "531039200bdb72d760ad",
+    "url": "/lets-watch-sailormoon/static/js/1.53103920.chunk.js"
+  },
+  {
+    "revision": "3fb44188e03cb1fd042a",
+    "url": "/lets-watch-sailormoon/static/css/main.8c7a976e.chunk.css"
+  },
+  {
+    "revision": "1e82152f12cae83e01aa0699a2ad55db",
+    "url": "/lets-watch-sailormoon/index.html"
+  }
+];
