919b75911c0e985ffe3f3fb191c8ed508761693f
Nathan Beck <njosefbeck@gmail.com>
2019-05-24 08:04:26 -0500

    Updates

---
+++ precache-manifest.8e7e9e2e3dc958edd310999e54e88c6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9782c4e5c568261e50151d5b380f1c65",
+    "url": "/lets-watch-sailormoon/index.html"
+  },
+  {
+    "revision": "41fa354b968a2f82967a",
+    "url": "/lets-watch-sailormoon/static/css/main.5d60e834.chunk.css"
+  },
+  {
+    "revision": "e34e57f5fcaf1bd89751",
+    "url": "/lets-watch-sailormoon/static/js/2.7ce5f5dc.chunk.js"
+  },
+  {
+    "revision": "41fa354b968a2f82967a",
+    "url": "/lets-watch-sailormoon/static/js/main.6954f58d.chunk.js"
+  },
+  {
+    "revision": "7ec8520eb030c1205c5b",
+    "url": "/lets-watch-sailormoon/static/js/runtime~main.b45cb42b.js"
+  },
+  {
+    "revision": "1630c2ed742f926e7fded2eae58687ee",
+    "url": "/lets-watch-sailormoon/static/media/sailor_moon_logo.1630c2ed.png"
+  }
+]);
